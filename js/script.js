b9e9d9cf3be4e38b43cd2c19dd3c6421d71b9d36
danxyz <d4nielturner@gmail.com>
2024-11-19 13:09:04 +0000

    Basic UI features added

--- js/script.js
+++ js/script.js
@@ -3,28 +3,33 @@ const rock = document.getElementById('rock')
 const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
 const startBtn = document.getElementById('btn-start')
+const rps = document.querySelectorAll('.flex-item')
+const playerSpan = document.getElementById('player-span')
+const compSpan = document.getElementById('computer-span')
+const msgDiv = document.getElementById('msg-div')
+const msgTxt = document.getElementById('msg-text')
+
 
 let playerChoice = ''
-let computerChoice = ''
+let compChoice = ''
 let playerScore = 0
-let computerScore = 0
-let totalScore = `Your score: ${playerScore} - Computer Score: ${computerScore}`
+let compScore = 0
+let totalScore = `Your score: ${playerScore} - Computer Score: ${compScore}`
 
 function getComputerChoice(max) {
     let num = Math.floor(Math.random() * max);
     if (num == 1) {
-            computerChoice = 'rock'
+            compChoice = 'rock'
     }   else if (num == 2) {
-            computerChoice = 'paper'
+            compChoice = 'paper'
     }   else {
-            computerChoice = 'scissors'
+            compChoice = 'scissors'
     }
 }
 
 // Get user choice
 
 container.addEventListener('click', ev => {
-    let rps = document.querySelectorAll('.flex-item')
     if (ev.target.closest('.flex-item')) {
         for (i of rps) {
             i.classList.remove('active-item')
@@ -36,52 +41,56 @@ container.addEventListener('click', ev => {
 
 
 
-function checkWinner(playerChoice, computerChoice) {
-    if ((playerChoice == 'rock') && (computerChoice == 'scissors')) {
-            console.log('You win! Rock beats scissors')
+function checkWinner(playerChoice, compChoice) {
+    if ((playerChoice == 'rock') && (compChoice == 'scissors')) {
+            msgTxt.textContent = 'You win! Rock beats scissors'
             playerScore++
     } 
-        else if ((playerChoice == 'rock') && (computerChoice == 'paper')) {
-            console.log('You Lose! Paper beats rock')
-            computerScore++
+        else if ((playerChoice == 'rock') && (compChoice == 'paper')) {
+            msgTxt.textContent = 'You Lose! Paper beats rock'
+            compScore++
     }    
-        else if ((playerChoice == 'paper') && (computerChoice == 'rock')) {
-            console.log('You win! Paper beats rock')
+        else if ((playerChoice == 'paper') && (compChoice == 'rock')) {
+            msgTxt.textContent = 'You win! Paper beats rock'
             playerScore++
     }
-        else if ((playerChoice == 'paper') && (computerChoice == 'scissors')) {
-            console.log('You lose! scissors beats paper')
-            computerScore++
+        else if ((playerChoice == 'paper') && (compChoice == 'scissors')) {
+            msgTxt.textContent = 'You lose! scissors beats paper'
+            compScore++
     }
-        else if ((playerChoice == 'scissors') && (computerChoice == 'paper')) {
-            console.log('You win! scissors beats paper')
+        else if ((playerChoice == 'scissors') && (compChoice == 'paper')) {
+            msgTxt.textContent = 'You win! scissors beats paper'
             playerScore++
     }
-        else if ((playerChoice == 'scissors') && (computerChoice == 'rock')) {
-            console.log('You lose! rock beats paper')
-            computerScore++
-    }   else if ((playerChoice == computerChoice)) {
-            console.log(`You both picked ${playerChoice}, try again`)
+        else if ((playerChoice == 'scissors') && (compChoice == 'rock')) {
+            msgTxt.textContent = 'You lose! rock beats paper'
+            compScore++
+    }   else if ((playerChoice == compChoice)) {
+            msgTxt.textContent = (`You both picked ${playerChoice}, try again`)
     }   else ('Error something went wrong')
-    totalScore = `Your score: ${playerScore} - Computer Score: ${computerScore}`
+    playerSpan.textContent = `${playerScore}`
+    compSpan.textContent = `${compScore}`
 }
 
 // Start round
 
 startBtn.addEventListener('click', function() {
     getComputerChoice(3)
-    checkWinner(playerChoice, computerChoice)
-    computerChoice = ''
+    checkWinner(playerChoice, compChoice)
+    compChoice = ''
     playerChoice = ''
+    for (i of rps) {
+        i.classList.remove('active-item')
+    }
     console.log(totalScore)
     if ((playerScore) == 5) {
-        console.log(`Congratulations you're the champion of Rock, Paper, Scissors`)
-        playerScore = 0
-        computerScore = 0
-    } else if ((computerScore) == 5) {
-        console.log(`You lost to the machine`)
-        playerScore = 0
-        computerScore = 0
+        msgTxt.textContent = `Congratulations you're the winner!`
+        playerSpan.textContent = `0`
+        compSpan.textContent = `0`
+    } else if ((compScore) == 5) {
+        msgTxt.textContent = `Sorry, you lose`
+        playerSpan.textContent = `0`
+        compSpan.textContent = `0`
     }
 })
 
